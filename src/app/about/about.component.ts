bf42c4e8332787464c12e4dfcb63d721e301218a
Yamil Andrade <yamil.andrade@gmail.com>
2025-05-05 15:27:28 -0400

    PreparativosProyectoFinal

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
